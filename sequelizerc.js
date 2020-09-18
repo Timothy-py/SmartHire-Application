7e5f7cc473397f576381932dc9113ef8eba0a4aa
Timothy-py <adeyeyetimothy33@gmail.com>
2020-09-18 13:49:15 +0100

    :hammer: setup and configured the DB, postgres db and sequelize ORM

---
+++ sequelizerc.js
@@ -0,0 +1,11 @@
+// this file consists of path to files required by sequelize
+
+const path =  require("path");
+
+
+module.exports = {
+    "config": path.resolve('./dbServer/config', 'config.json'), //contains app config settings e.g DB auth config
+    "models-path": path.resolve('./dbServer/models'), // holds the application models
+    "seeders-path": path.resolve('./dbServer/seeders'), // holds seed data
+    "migrations-path": path.resolve('./dbServer/migrations') // contains app migrations
+};
