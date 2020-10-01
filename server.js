a06ba99bd1c591b2990b10985e5d6a5b945c174b
Timothy-py <adeyeyetimothy33@gmail.com>
2020-10-01 19:46:31 +0100

    :poop: created a page not found error api response

--- server.js
+++ server.js
@@ -56,5 +56,24 @@ app.get('/logout', (req, res, next)=>{
     })
 })
 
+// catch 404 and forward to error handler
+app.use((req, res, next)=>{
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next)=>{
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // return a 404 response
+    res.status(err.status || 500).send({
+        message: "Page can not be found!"
+    })
+}) 
+
 // export the app
 module.exports = app;
